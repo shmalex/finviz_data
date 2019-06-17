96b99704ee692d6fe69c47c1d17618e6fd9ba1d9
Alexei Matusevski <alexei.matusevski@sema4genomics.com>
2019-06-17 13:49:15 -0400

    updates

--- git-commit.sh
+++ git-commit.sh
@@ -1,3 +1,11 @@
+cd ../mybdds
+bash run-loader.sh
+bash run-loader.sh
+bash run-loader.sh
+bash run-loader.sh
+bash run-loader.sh
+bash run-loader.sh
+cd ../finviz_data
 git add *
 git commit -m 'updates'
 git push
